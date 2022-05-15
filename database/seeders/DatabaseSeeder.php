40f4d1614df2e1c9295843886bb2af56f6f62d28
Bearnerd Bondly <bearnerd@gmail.com>
2022-05-15 11:14:53 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CountrySeeder::class);
+        $this->call(DaerahSeeder::class);
+        $this->call(JenisDibawaKeluarSeeder::class);
+        $this->call(JenisDibawaMasukSeeder::class);
+        $this->call(JenisPerniagaanDagingSeeder::class);
+        $this->call(JenisPerniagaanHaiwanSeeder::class);
         $this->call(KaedahUlasanSeeder::class);
         $this->call(KeputusanUjianSeeder::class);
+        $this->call(MediumPenghantaranSeeder::class);
         $this->call(PejabatPembayaranSeeder::class);
         $this->call(PejabatPungutanLesenSeeder::class);
+        $this->call(PejabatTableSeeder::class);
+        $this->call(PermitTypeSeeder::class);
         $this->call(SoalanEphlSeeder::class);
         $this->call(StatusBorangSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(DaerahSeeder::class);
     }
 }
