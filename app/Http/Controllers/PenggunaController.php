7472d461c9f72c42764d4488fd29d503d6232200
Bearnerd Bondly <bearnerd@gmail.com>
2022-05-16 22:47:41 +0800

    Update PenggunaController.php

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -16,7 +16,7 @@ class PenggunaController extends Controller
             return view('users');
         }else{
             return view('auth.login');
-        }         
+        }
     }
 
     public function loadUserDatatable(){
@@ -32,13 +32,13 @@ class PenggunaController extends Controller
             'ic_number' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8'],
-            'role' => ['required','in:admin,normal,guest,client'], 
+            'role' => ['required','in:admin,normal,guest,client'],
         ]);
 
         //Insert new record if no errors, else return errors
         if($validator->fails()){
             return response()->json(array('status'=>'fails','errors'=>$validator->errors()));
-        }else{            
+        }else{
             $rec = new User;
             $rec->name = request('name');
             $rec->ic_number = request('ic_number');
@@ -48,8 +48,64 @@ class PenggunaController extends Controller
             $rec->save();
 
             $result = DB::select(DB::raw("SELECT users.*,DATE_FORMAT(updated_at,'%d/%m/%Y %h:%i%p') AS update_date FROM users"));
-            
+
             return response()->json(array('status'=>'success','result'=>$result));
-        }            
+        }
+    }
+
+    public function postUpdateUser(){
+        dd(request()->all());
+
+        if(request('password_edit')){
+            if(Auth::user()->role = "super admin"){
+                $validator = Validator::make(request()->all(), [
+                    'name' => ['required', 'string', 'max:255'],
+                    'ic_number' => ['required', 'string', 'max:255', 'unique:users'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                    'password' => ['required', 'string', 'min:8'],
+                ]);
+            }else{
+                $validator = Validator::make(request()->all(), [
+                    'name' => ['required', 'string', 'max:255'],
+                    'ic_number' => ['required', 'string', 'max:255', 'unique:users'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                    'password' => ['required', 'string', 'min:8'],
+                    'role' => ['required','in:admin,normal,guest,client'],
+                ]);
+            }
+        }else{
+            if(Auth::user()->role = "super admin"){
+                $validator = Validator::make(request()->all(), [
+                    'name' => ['required', 'string', 'max:255'],
+                    'ic_number' => ['required', 'string', 'max:255', 'unique:users'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                ]);
+            }else{
+                $validator = Validator::make(request()->all(), [
+                    'name' => ['required', 'string', 'max:255'],
+                    'ic_number' => ['required', 'string', 'max:255', 'unique:users'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                    'role' => ['required','in:admin,normal,guest,client'],
+                ]);
+            }
+        }
+
+
+        //Insert new record if no errors, else return errors
+        if($validator->fails()){
+            return response()->json(array('status'=>'fails','errors'=>$validator->errors()));
+        }else{
+            $rec = new User;
+            $rec->name = request('name');
+            $rec->ic_number = request('ic_number');
+            $rec->email = request('email');
+            $rec->password = Hash::make(request('password'));
+            $rec->role = request('role');
+            $rec->save();
+
+            $result = DB::select(DB::raw("SELECT users.*,DATE_FORMAT(updated_at,'%d/%m/%Y %h:%i%p') AS update_date FROM users"));
+
+            return response()->json(array('status'=>'success','result'=>$result));
+        }
     }
 }
